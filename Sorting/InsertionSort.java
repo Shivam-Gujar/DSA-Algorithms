3d87323fce8fbb4fe110599bf217bb889994d7d0
Shivam Gujar <shivamgujar06@gmail.com>
2025-10-02 02:19:29 +0530

    Update InsertionSort.java

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -9,14 +9,18 @@ public class InsertionSort {
     }
     static int[] sort(int[] arr) {
         for (int i = 1; i < arr.length; i++) {
+            
             for (int j = i; j > 0; j--) {
+                
                 if (arr[j] < arr[j - 1]) {
                     int swap = arr[j];
                     arr[j] = arr[j - 1];
                     arr[j - 1] = swap;
+                    
                 } else break;
             }
         }
         return arr;
     }
 }
+
